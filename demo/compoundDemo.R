7a1fcf21c5bb74686b998605af84617efdad4d9f
Tim Dorscheidt <tim@thehyve.nl>
2015-04-24 15:06:30 +0200

    Compound demo scripts now point to tl demo server
    Also added tag construction for second study

--- demo/compoundDemo.R
+++ demo/compoundDemo.R
@@ -1,8 +1,9 @@
 require("RPostgreSQL")
+require("transmartRClient")
 
 # Setup connection to local transmart server and database
-connectToTransmart("http://localhost:8080/transmart")
-dbConnection <- dbConnect(PostgreSQL(), host = "localhost", port = "5432", user="timdo", password="timdo", dbname="transmart")
+connectToTransmart("http://lobster.nd4.thehyve.net/transmart")
+dbConnection <- dbConnect(PostgreSQL(), host = "localhost", port = "5433", user="tm_cz", password="tm_cz", dbname="transmart")
 
 # Fetch subjects with treatment-concepts linked to certain compound (across studies)
 getSubjectsByCompound(dbConnection, "c1")
--- demo/setupDatabaseForCompoundDemo.R
+++ demo/setupDatabaseForCompoundDemo.R
@@ -1,12 +1,12 @@
-connectToTransmart("http://localhost:8080/transmart")
-localEnvironment <- transmartClientEnv
+connectToTransmart("http://lobster.nd4.thehyve.net/transmart")
 
 studyName <- "RA_NZEUSSEU_GSE36700"
 concepts <- getConcepts(studyName)
 tagKeyName <- "Compound collaborator ID"
 
 require("RPostgreSQL")
-con <- dbConnect(PostgreSQL(), host = "localhost", port = "5432", user="timdo", password="timdo", dbname="transmart")
+con <- dbConnect(PostgreSQL(), host = "localhost", port = "5433", user="tm_cz", password="tm_cz", dbname="transmart")
+
 dbSendQuery(con, paste(sep = "",
         "insert into i2b2metadata.i2b2_tags(tag_id, path, tag, tag_type, tags_idx)
         values (1, '", concepts[grep("^colchicine$", concepts$name),]$fullName,
@@ -19,5 +19,14 @@ dbSendQuery(con, paste(sep = "",
        "insert into i2b2metadata.i2b2_tags(tag_id, path, tag, tag_type, tags_idx)
         values (1, '", concepts[grep("^NSAIDs and colchicine$", concepts$name),]$fullName,
        "', 'c2+c1', '", tagKeyName, "', 1);"))
+
+studyName <- "RA_Raterman_GSE37107"
+concepts <- getConcepts(studyName)
+tagKeyName <- "Compound collaborator ID"
+
+dbSendQuery(con, paste(sep = "",
+        "insert into i2b2metadata.i2b2_tags(tag_id, path, tag, tag_type, tags_idx)
+        values (1, '", concepts[grep("^baseline$", concepts$name),]$fullName,
+        "', 'c1', '", tagKeyName, "', 1);"))
 
 postgresqlCloseConnection(con)
